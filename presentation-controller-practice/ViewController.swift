69f1104b38cd386ff0492cdc7408d63113bf18ea
Shima <shima.jin@icloud.com>
2018-06-29 15:19:32 +0900

    wip

--- presentation-controller-practice/ViewController.swift
+++ presentation-controller-practice/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
+
+class MyPresentationController: UIPresentationController {
+    
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let button = UIButton()
+        button.setTitle("Present", for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 100, height: 60)
+        button.center = view.center
+        button.layer.cornerRadius = 8.0
+        button.clipsToBounds = true
+        button.backgroundColor = .lightGray
+        view.addSubview(button)
+
+        button.addTarget(self, action: #selector(self.tappedButton(_:)), for: .touchUpInside)
+
     }
 
+    @objc func tappedButton(_ selector: UITapGestureRecognizer) {
 
+        print("tapped")
+
+        let viewController = UIViewController()
+        viewController.modalPresentationStyle = .custom
+        viewController.transitioningDelegate = self
+        viewController.view.backgroundColor = UIColor.darkGray.withAlphaComponent(0.2)
+        present(viewController, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return MyPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
